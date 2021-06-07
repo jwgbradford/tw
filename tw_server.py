bfd670761336e83e7f33973995db6841f4f5eb05
Ade-StapleHill <ade.perry@digilocal.org.uk>
2021-06-07 19:00:04 +0100

    Development - add lock release in server to chat to multi client.
    Crash when second client connected. Files added:
    
    tw_server.py:
      Added thread local namespace, did not improve things
      Added into the forever loop a lock acquire and release for timeslicing
    tw_network.py:
       server ip "localhost"
    ade1_bug.txt
        Describe problem when multi client connected.

--- tw_server.py
+++ tw_server.py
@@ -5,45 +5,53 @@ from tw_game_engine import GameEngine as tw_ge
 class TinyWorld:
     def __init__(self):
         self.game_engine = tw_ge()
+        self.ns = threading.local() # namespace for thread local data
 
     def threaded_client(self, conn):
         pygame.init()
-        clock = pygame.time.Clock()
+        self.ns.clock = pygame.time.Clock()
 
         # get player id from new connection
-        my_id = json.loads(conn.recv(2048))
-        print('Player id:', my_id)
-        self.game_engine.add_player(my_id)
+        self.ns.my_id = json.loads(conn.recv(2048))
+        print('Player id:', self.ns.my_id)
+        self.game_engine.add_player(self.ns.my_id)
 
         # send initial data to new connection
-        starting_data = self.game_engine.update_objects()
-        json_data = json.dumps(starting_data)
-        conn.send(json_data.encode())
+        self.ns.starting_data = self.game_engine.update_objects()
+        self.ns.json_data = json.dumps(self.ns.starting_data)
+        conn.send(self.ns.json_data.encode())
+        self.print_lock.release()
 
         while True:
-            try:
-                keys = json.loads(conn.recv(2048))
-                if not keys:
-                    print('Connection closed')
-                    break
-                self.game_engine.move_player(my_id, keys)
-                self.game_engine.move_ai()
+            self.print_lock.acquire()
 
-                # send new game data to player connection
-                data = self.game_engine.update_objects()
-                json_data = json.dumps(data)
-                conn.send(json_data.encode())
+            try:
+                self.ns.data = conn.recv(2048)
+                keys = {}
+                if len(self.ns.data) > 0:
+                    keys = json.loads(self.ns.data)
+                if not keys:
+                    True # print('Connection closed')
+                else:
+                    self.game_engine.move_player(self.ns.my_id, keys)
+                    self.game_engine.move_ai()
+
+                    # send new game data to player connection
+                    self.ns.data = self.game_engine.update_objects()
+                    self.ns.json_data = json.dumps(self.ns.data)
+                    conn.send(self.ns.json_data.encode())
             except socket.error as e:
                 print(e)
                 break
-            clock.tick(60)
+            self.print_lock.release()
+            self.ns.clock.tick(60)
         self.print_lock.release()
         print("Lost connection")
         conn.close()
 
     def main(self):
         # local ip address
-        server = ""
+        server = "localhost"
         port = 5555
         # set up threading lock
         self.print_lock = threading.Lock()
